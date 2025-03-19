5c1c747eb391efd2b0e26c9f8f433d97de6f57e1
Alex Rako <147698888+Rako75@users.noreply.github.com>
2025-03-19 14:33:53 +0100

    Update Montant_pret.py

--- Montant_pret.py
+++ Montant_pret.py
@@ -4,10 +4,6 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
-
-
-
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet, LogisticRegression
